bff642e6e1d339490cc53a337a7dc2b31e7beb8c
Alvin-gamer <alvinmingze2012@gmail.com>
2026-03-01 19:45:47 -0300

    Delete sound.js

--- sound.js
+++
@@ -1,9 +0,0 @@
-// ADD THIS - safe audio playback function
-function playSound(soundId) {
-    let sound = document.getElementById(soundId);
-    if (sound) {
-        sound.currentTime = 0;
-        sound.play().catch(e => {
-        });
-    }
-}
